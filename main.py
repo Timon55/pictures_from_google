5b9f9c07518c97e12f30d07f0e18a37f3902b8da
Timon55 <forjob55@bk.ru>
2022-05-16 23:57:55 +0700

    Добавил README

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ url = f"https://www.google.com/search?tbm=isch&q={search_object}"
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html.parser')
 list_images = soup.findAll('img')
-list_images = list_images[1:16]
+list_images = list_images[1:16] # нулевая картинка логотип в шапке с локальным адресом
 i = 1
 if not os.path.exists(f"{search_object}"):
     os.mkdir(f"{search_object}")
